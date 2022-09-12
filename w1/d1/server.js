620f5bb605724747c0099600cc1322226a7b42ce
umuterbas <97316464+umuterbas@users.noreply.github.com>
2022-09-12 09:21:49 -0700

    a

--- w1/d1/server.js
+++ w1/d1/server.js
@@ -1,11 +1,21 @@
 const http = require("http");
 
+const users = [{ umut, vini, kento }];
+
 const server = http.createServer((request, response) => {
   console.log("A wild client appears!", request.url);
 
-  if (request.url === "/test") {
+  if (request.url === "/dogs") {
     response.writeHead(200, { "Content-type": "text/html" });
-    response.write("<h1>Hello Client in test!!!!!!</h1>");
+    response.write(
+      "<img src='https://pbs.twimg.com/profile_images/1213144769452486656/sRfg9m1O_400x400.jpg' alt=''>"
+    );
+  }
+  if (request.url === "/users") {
+    response.writeHead(200, { "Content-type": "application/json" });
+    response.write(
+      "<p></p>"
+    );
   }
   response.end();
 });
